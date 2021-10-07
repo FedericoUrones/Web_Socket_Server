4ccd16df68d2381a09795aeee25537abf7d9c18e
Federico Urones <federicourones@gmail.com>
2021-10-07 12:57:25 -0300

    changes

--- models/server.js
+++ models/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { Socket } = require('socket.io');
+
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
-        this.app    = express();
-        this.port   = process.env.PORT;
+        this.app = express();
+        this.port = process.env.PORT;
         this.server = require('http').createServer(this.app);
-        this.io     = require('socket.io')(this.server);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {}
 
@@ -16,6 +19,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Config de Sockets
+        this.sockets()
     }
 
     async conectarDB() {
@@ -26,22 +32,27 @@ class Server {
     middlewares() {
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         // Directorio Público
-        this.app.use( express.static('public') );
+        this.app.use(express.static('public'));
 
     }
 
     routes() {
-        
+
         // this.app.use( this.paths.auth, require('../routes/auth'));
-        
+
+    }
+
+    sockets() {
+        this.io.on('connection', socketController);
+
     }
 
     listen() {
-        this.server.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
+        this.server.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
         });
     }
 
